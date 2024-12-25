97251fbcbb9cde0de35d39ba55e3304e36fdba92
Vineet Singraul <Singraulvineet20@gmail.com>
2024-12-25 23:09:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,91 @@
+var img1 = document.querySelector("#first_div");
+var img2 = document.querySelector("#img1_Div");
+var img3 = document.querySelector("#img2_Div");
+var img4 = document.querySelector("#img3_Div");
+
+change1=()=> {
+    img1.src = img2.src;
+}
+
+change2=()=> {
+    img1.src = img3.src;
+}
+
+change3=()=> {
+    img1.src = img4.src;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
